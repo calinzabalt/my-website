92e1b4155e51b12ea1443f1b68c88fa9865ccee6
calinzabalt <calinzabalt@gmail.com>
2018-03-27 21:25:25 +0300

    another push

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,17 +6,33 @@ export default class Contact extends Component {
     render() {
         return (
         
-<div className="container">
+<div className="container contact">
 
     <div className="box-2">
     <h2>I’m currently available for freelance work.</h2>
       <p>If you have a project that you want to get started, or if you want my help to accomplish something you can contact my on this platforms:</p>
-      <a href="https://web.facebook.com/calinviorel.cozma" className="fa fa-facebook"></a>
-      <a href="https://www.linkedin.com/in/cozma-viorel-calin-947658145/" className="fa fa-linkedin"></a>
-      <a href="https://www.youtube.com/channel/UCQ2N3yvA9bq0ZgXEK2m8pwA?view_as=subscriber" className="fa fa-youtube"></a>
-      <a href="https://plus.google.com/u/0/112508366335698318230" className="fa fa-google"></a>
+      <a href="https://web.facebook.com/calinviorel.cozma" target="blank" className="fa fa-facebook"></a>
+      <a href="https://www.linkedin.com/in/cozma-viorel-calin-947658145/" target="blank" className="fa fa-linkedin"></a>
+      <a href="https://www.youtube.com/channel/UCQ2N3yvA9bq0ZgXEK2m8pwA?view_as=subscriber" target="blank" className="fa fa-youtube"></a>
+      <a href="https://plus.google.com/u/0/112508366335698318230" target="blank" className="fa fa-google"></a>
 </div>
+<form method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
 </div>
+
 
 
 
